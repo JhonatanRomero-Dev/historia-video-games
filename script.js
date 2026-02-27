45cf7fc0151d1fde10d66a9bf0e10831bafa679d
Jhondev-404 <jhonatan555romero@gmail.com>
2026-02-27 11:15:47 -0300

    Corrigindo estrutura das sections e preparando para reveal animation

--- script.js
+++ script.js
@@ -44,3 +44,65 @@ document.getElementById("searchInput").addEventListener("keypress", function(e) 
     searchGame();
   }
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  const form = document.querySelector(".search-form");
+
+  if (!form) return; // segurança
+
+  const input = form.querySelector("input");
+  const message = document.getElementById("redirectMessage");
+  const text = document.getElementById("redirectText");
+
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let game = input.value.trim();
+    if (!game) return;
+
+    let firstChar = game.charAt(0).toUpperCase();
+    let url;
+
+    if (!isNaN(firstChar)) {
+      url = "https://vimm.net/vault/?p=list&system=PS2&section=number";
+      text.innerText = `🎮 Redirecionando para jogos que começam com número...`;
+    } 
+    else if (/^[A-Z]$/.test(firstChar)) {
+      url = "https://vimm.net/vault/PS2/" + firstChar;
+      text.innerText = `🎮 Redirecionando para jogos com a letra ${firstChar}...`;
+    } 
+    else {
+      return;
+    }
+
+    message.classList.add("show");
+
+    setTimeout(() => {
+      window.location.href = url;
+    }, 2000);
+
+  });
+
+});
+
+// Scroll Reveal
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+
+  reveals.forEach((element) => {
+    const elementTop = element.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 100) {
+      element.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+
+// Executa ao carregar
+revealOnScroll();
